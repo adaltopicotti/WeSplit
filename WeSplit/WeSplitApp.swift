7b99955dddba963b17ba6af7fb4543e367b472d8
Adalto Picotti Junior <adaltocotti.jr@gmail.com>
2023-01-25 22:39:08 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adalto Picotti Junior on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
